9ec9c6ee2483be8a193d22fd39018935130a9f6e
WingedBread <jmh952010@gmail.com>
2018-01-30 12:19:12 +0100

    Trap Behaviour with Koreography
    30/01

---
+++ Assets/Dungeons_Dancers/Scripts/PlayerEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class PlayerEvent : MonoBehaviour
+{
+    private Vector3 playerTransform;
+
+    // Use this for initialization
+    void Start()
+    {
+        Koreographer.Instance.RegisterForEvents("PlayerEvent", ChangeSize);
+
+        playerTransform = new Vector3(1, 1, 1);
+    }
+
+    void ChangeSize(KoreographyEvent kPlayerEvent)
+    {
+        Debug.Log("playerworking");
+
+        if (this.transform.localScale == playerTransform)
+        {
+            this.transform.localScale = new Vector3(1.2f, 1, 1);
+        }
+        else this.transform.localScale = playerTransform;
+
+    }
+}
---
+++ Assets/Dungeons_Dancers/Scripts/TrapEvent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class TrapEvent : MonoBehaviour {
+
+    private Material mat;
+    [Header("Choose Trap Behaviour from 1-3")]
+    [SerializeField]
+    private int trapBehaviour;
+
+	// Use this for initialization
+	void Start () 
+    {
+        switch (trapBehaviour)
+        {
+            case 1:
+                Koreographer.Instance.RegisterForEvents("Trap1Event", ChangeColor);
+                break;
+            case 2:
+                Koreographer.Instance.RegisterForEvents("Trap2Event", ChangeColor);
+                break;
+            case 3:
+                Koreographer.Instance.RegisterForEvents("Trap3Event", ChangeColor);
+                break;
+        }
+        mat = this.GetComponent<MeshRenderer>().material;
+	}
+	
+    void ChangeColor(KoreographyEvent kTrapEvent)
+    {
+        Debug.Log("trapworking");
+
+        if (mat.color == Color.black) mat.color = Color.white;
+        else mat.color = Color.black;
+    }
+}
