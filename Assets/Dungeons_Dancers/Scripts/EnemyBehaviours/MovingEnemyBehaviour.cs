1eca13ef58be28902dcab49bcf00e05a82e2f6f1
WingedBread <jmh952010@gmail.com>
2018-05-09 19:02:43 +0200

    Skeleton Logic :100:  :skull:

--- Assets/Dungeons_Dancers/Scripts/EnemyBehaviours/MovingEnemyBehaviour.cs
+++ Assets/Dungeons_Dancers/Scripts/EnemyBehaviours/MovingEnemyBehaviour.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingEnemyBehaviour : MonoBehaviour {
-    
-    public Transform[] points;
-    private int destPoint = 0;
+
+    [Header("Moves Horizontally?")]
+    [SerializeField]
+    private bool xAxis;
+    [Header("Initial Direction")]
+    [SerializeField]
+    private int direction = -1;
 
     [Header("Choose Trap Behaviour from 1-3")]
     [SerializeField]
@@ -16,30 +20,47 @@ public class MovingEnemyBehaviour : MonoBehaviour {
 
     private bool activeTrapEvent;
 
-    void Start()
+    private void Start()
     {
-        
+        if (xAxis)
+        {
+            if(direction == 1)
+            {
+                SetRotation(1);
+            }
+            else if(direction == -1)
+            {
+                SetRotation(0);
+            }
+        }
+        else
+        {
+            if (direction == 1)
+            {
+                SetRotation(3);
+            }
+            else if (direction == -1)
+            {
+                SetRotation(2);
+            }
+        }
     }
-
-    void Update()
-    {
-        //if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            //GotoNextPoint();
-    }
-
     public void ActiveTrap()
     {
+        if (xAxis)
+        {
+            this.transform.position = new Vector3(this.transform.position.x + (direction), this.transform.position.y, this.transform.position.z);
+        }
+        else
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + (direction));
+        }
+
         activeTrapEvent = true;
     }
     public void DisableTrap()
     {
         activeTrapEvent = false;
-    }
-
-    void GotoNextPoint()
-    {
-        //agent.destination = points[destPoint].position;
-        //destPoint = (destPoint + 1) % points.Length;
     }
 
     private IEnumerator ReturnIdle(float time)
@@ -57,9 +78,53 @@ public class MovingEnemyBehaviour : MonoBehaviour {
         return activeTrapEvent;
     }
 
-
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider col)
     {
-        //Collide with directional platform
+        if(col.gameObject.tag == "EnemyDirection")
+        {
+            if (col.gameObject.transform.GetChild(0).name == "Up")
+            {
+                direction = 1;
+                SetRotation(3);
+                xAxis = false;
+            }
+            else if (col.gameObject.transform.GetChild(0).name == "Down")
+            {
+                direction = -1;
+                SetRotation(2);
+                xAxis = false;
+            }
+            else if (col.gameObject.transform.GetChild(0).name == "Right")
+            {
+                direction = 1;
+                SetRotation(1);
+                xAxis = true;
+            }
+            else if (col.gameObject.transform.GetChild(0).name == "Left")
+            {
+                direction = -1;
+                SetRotation(0);
+                xAxis = true;
+            }
+        }
+    }
+
+    void SetRotation(int direction)
+    {
+        switch (direction)
+        {
+            case 0: //LEFT
+                transform.GetChild(0).rotation = Quaternion.Euler(0, 90, 45);
+                break;
+            case 1: //RIGHT
+                transform.GetChild(0).rotation = Quaternion.Euler(0, -90, -45);
+                break;
+            case 2: //DOWN
+                transform.GetChild(0).rotation = Quaternion.Euler(45, 0, 0);
+                break;
+            case 3: //UP
+                transform.GetChild(0).rotation = Quaternion.Euler(-45, 180, 0);
+                break;
+        }
     }
 }
