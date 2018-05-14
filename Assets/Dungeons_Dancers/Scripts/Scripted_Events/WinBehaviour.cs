d664e2e94de0c3594137fc71605918719cb0e22d
WingedBread <jmh952010@gmail.com>
2018-05-14 18:04:19 +0200

    Wins Behavs

---
+++ Assets/Dungeons_Dancers/Scripts/Scripted_Events/WinBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinBehaviour : MonoBehaviour {
+
+	[SerializeField]
+    private GameObject winParticles;
+
+	private Camera mainCamera;
+
+	[SerializeField]
+	private Sprite[] lights = new Sprite[3];
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
