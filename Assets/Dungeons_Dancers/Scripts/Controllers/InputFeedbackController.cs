eebd58b0accb8ef79d172aad4b454040b7722ca1
WingedBread <jmh952010@gmail.com>
2018-05-21 16:35:10 +0200

    Estela Player more things

--- Assets/Dungeons_Dancers/Scripts/Controllers/InputFeedbackController.cs
+++ Assets/Dungeons_Dancers/Scripts/Controllers/InputFeedbackController.cs
@@ -10,24 +10,26 @@ public class InputFeedbackController : MonoBehaviour {
 
     [Header("Sprites Feedback")]
     [SerializeField]
-    Sprite correctFloorSprite;
+    GameObject correctFloorSprite;
     [SerializeField]
-    Sprite incorrectFloorSprite;
+    GameObject incorrectFloorSprite;
 
     [Header("Duration Fade Sprites")]
     [SerializeField]
     float spriteDuartion;
 
+    [SerializeField]
     ParticleSystem textParticle;
+    [SerializeField]
     ParticleSystem lightParticle;
 
     string moveText;
 
     [SerializeField]
-    GameObject instantiatedGO;
+    List<GameObject> trailGO = new List<GameObject>();
 
-    GameObject[] trailGO;
-
+    private GameObject instantiatedGO;
+    private GameObject badGO;
 
 
 	// Use this for initialization
@@ -37,25 +39,36 @@ public class InputFeedbackController : MonoBehaviour {
 	
     public void CorrectFeedbackBehaviour()
     {
+        trailGO.Add((GameObject)Instantiate(correctFloorSprite, transform.parent.parent));
+        instantiatedGO = trailGO[trailGO.Count - 1];
+        instantiatedGO.transform.position = new Vector3(transform.position.x, instantiatedGO.transform.position.y, transform.position.z);
+        textParticle = instantiatedGO.transform.GetChild(0).GetComponent<ParticleSystem>();
+        lightParticle = instantiatedGO.transform.GetChild(1).GetComponent<ParticleSystem>();
+
+        //Easing Fade Out Correct
         switch(gameManager.GetRhythmAccuracy())
         {
             case 0: //Good
                 moveText = "Good";
-                
                 break;
             case 1: //Perfect
                 moveText = "Perfect!";
-                
                 break;
             case 2: //Great
                 moveText = "Great";
-
                 break;
         }
     }
 
     public void IncorrectFeedbackBehaviour()
     {
-        Instantiate(incorrectFloorSprite, transform);
+        badGO = Instantiate(incorrectFloorSprite, transform.parent.parent);
+        badGO.transform.position = new Vector3(transform.position.x, instantiatedGO.transform.position.y, transform.position.z);
+
+        for (int i = 0; i < trailGO.Count; i++){
+            Destroy(trailGO[i]);
+            //StartCoroutine - Easing Fade Incorrect
+            if (i == (trailGO.Count - 1)) trailGO.Clear();
+        }
     }
 }
