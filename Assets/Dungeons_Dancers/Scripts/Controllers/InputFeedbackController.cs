6115981d5649b39889260861061349d55715b3d9
WingedBread <jmh952010@gmail.com>
2018-05-22 21:07:53 +0200

    Particle Systems

--- Assets/Dungeons_Dancers/Scripts/Controllers/InputFeedbackController.cs
+++ Assets/Dungeons_Dancers/Scripts/Controllers/InputFeedbackController.cs
@@ -20,12 +20,17 @@ public class InputFeedbackController : MonoBehaviour {
 
     [SerializeField]
     ParticleSystem textParticle;
-    [SerializeField]
     ParticleSystem lightParticle;
 
-    string moveText;
-
     [SerializeField]
+    string goodText;
+    [SerializeField]
+    string greatText;
+    [SerializeField]
+    string perfectText;
+    [SerializeField]
+    Text particleText;
+
     List<GameObject> trailGO = new List<GameObject>();
 
     private GameObject instantiatedGO;
@@ -42,20 +47,19 @@ public class InputFeedbackController : MonoBehaviour {
         trailGO.Add((GameObject)Instantiate(correctFloorSprite, transform.parent.parent));
         instantiatedGO = trailGO[trailGO.Count - 1];
         instantiatedGO.transform.position = new Vector3(transform.position.x, instantiatedGO.transform.position.y, transform.position.z);
-        textParticle = instantiatedGO.transform.GetChild(0).GetComponent<ParticleSystem>();
-        lightParticle = instantiatedGO.transform.GetChild(1).GetComponent<ParticleSystem>();
+        lightParticle = instantiatedGO.transform.GetChild(0).GetComponent<ParticleSystem>();
 
         //Easing Fade Out Correct
         switch(gameManager.GetRhythmAccuracy())
         {
             case 0: //Good
-                moveText = "Good";
+                particleText.text = goodText;
                 break;
             case 1: //Perfect
-                moveText = "Perfect!";
+                particleText.text = perfectText;
                 break;
             case 2: //Great
-                moveText = "Great";
+                particleText.text = greatText;
                 break;
         }
     }
