340c05d891ee1576e71458d9920920ba22f103ad
clloses <info@curiallloses.com>
2018-05-22 22:21:10 +0200

    Més proves de UI diegètica.

---
+++ Assets/Dungeons_Dancers/Scripts/Controllers/TimerBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerBar : MonoBehaviour {
+
+	private GameManager gameManager;
+	private float initTimerWidth;
+	private float initTimer;
+	private float timerBarWidth;
+
+	[SerializeField]
+	private GameObject TimerBarObject;
+
+	// Use this for initialization
+	void Start () {
+		initTimerWidth = TimerBarObject.transform.localScale.x;
+		initTimer = 60f; //gameManager.GetDungeonTime ();
+		gameManager = GetComponent<GameManager>();
+		timerBarWidth = (gameManager.GetDungeonTime()*initTimerWidth)/initTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timerBarWidth = (gameManager.GetDungeonTime()*initTimerWidth)/initTimer;
+		TimerBarObject.transform.localScale = new Vector3(timerBarWidth,0.3f,1f);
+	}
+}
