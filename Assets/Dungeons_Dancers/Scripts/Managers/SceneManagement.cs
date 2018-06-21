b95921e691a16d3536fe12499f00da28528441f7
WingedBread <jmh952010@gmail.com>
2018-06-21 20:56:44 +0200

    Win texts

---
+++ Assets/Dungeons_Dancers/Scripts/Managers/SceneManagement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour {
+    
+    static string lastScene;
+    static string currentScene;
+
+    public static void ChangeScene(string sceneName)
+    {
+        lastScene = currentScene;
+        currentScene = sceneName;
+        SceneManager.LoadScene(currentScene);
+    }
+
+
+    public static void LoadLastScene()
+    {
+        string last = lastScene;
+        lastScene = currentScene;
+        currentScene = last;
+        SceneManager.LoadScene(currentScene);
+    }
+}
