6c1b005383ba7ffafcdf7ff1f240e0184678f983
WingedBread <jmh952010@gmail.com>
2018-07-31 14:31:33 +0200

    sparkle restart

--- Assets/Dungeons_Dancers/Scripts/Managers/PlayerManager.cs
+++ Assets/Dungeons_Dancers/Scripts/Managers/PlayerManager.cs
@@ -342,6 +342,14 @@ public class PlayerManager : MonoBehaviour
         {
             gameManager.levelEventsEasing4[i].SetPlayerState(PlayerStates.Succeed);
         }
+
+        for (int i = 0; i < collectibles.Count; i++)
+        {
+            collectibles[i].GetComponent<SparkleController>().Restart();
+        }
+        collectibles.Clear();
+        collectiblesController.Reset();
+
 		animator.SetBool("onWin", true);
 		inputController.SetRotation(2);
         gameManager.Win();
